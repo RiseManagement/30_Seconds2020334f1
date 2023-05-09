52c0bb5cc2767dc1f48ffb4178a62252ad0019b0
はまちゃん <hiroakipokemon@gmail.com>
2023-05-09 21:54:08 +0900

    クラス作成（簡易処理作成）

---
+++ Assets/Hama/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// タイマークラス
+/// </summary>
+
+public class Timer : MonoBehaviour
+{
+    //現在のカウント
+    float count;
+
+    //カウント最大値
+    [SerializeField] float countmax = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = countmax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //カウントダウン
+    void CountDown()
+    {
+        count = count - Time.deltaTime;
+    }
+}
