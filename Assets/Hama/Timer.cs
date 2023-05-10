f2f0900b8e1b58a6fcbe4f870856bd69badc5f4e
はまちゃん <hiroakipokemon@gmail.com>
2023-05-10 21:59:17 +0900

    タイマー処理完了、テスト用シーン作成 テストシーン 　ー時間をUIに表示

--- Assets/Hama/Timer.cs
+++ Assets/Hama/Timer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// タイマークラス
@@ -8,11 +9,14 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    // UI Text指定用
+    public Text UIText;
+
     //現在のカウント
     float count;
 
     //カウント最大値
-    [SerializeField] float countmax = 30;
+    [SerializeField] float countmax = 30.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +27,20 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UIText.text = count.ToString("f0");
+        CountDown();
+
+        if(count < 0)
+        {
+            Debug.Log("インターバル画面に遷移");
+        }
     }
 
     //カウントダウン
     void CountDown()
     {
-        count = count - Time.deltaTime;
+        count -= Time.deltaTime;
+
+        Debug.Log(count);
     }
 }
