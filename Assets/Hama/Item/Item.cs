02937fe9ca964cff318bcf34a29b89d98bb49119
はまちゃん <hiroakipokemon@gmail.com>
2023-05-25 23:40:56 +0900

    追加：正式データでCSVからアイテムデータを設定

---
+++ Assets/Hama/Item/Item.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+/// <summary>
+/// アイテムデータベース
+/// </summary>
+public class ItemDataBase : ScriptableObject
+{
+    public ItemData[] datas;
+}
+
+/// <summary>
+/// アイテムデータ
+/// </summary>
+[System.Serializable]
+public class ItemData
+{
+    public int ID;
+    public string Name;
+    public string Explanation;
+    public Image Image;
+    public int OwnerFlag;
+    public int InteractFlag;
+}
+
+/// <summary>
+/// CSVファイルインポート後アイテムデータ設定
+/// </summary>
+#if UNITY_EDITOR
+public class ItemInportDataSet : AssetPostprocessor
+{
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        foreach (string str in importedAssets)
+        {
+            //　IndexOfの引数は"/(読み込ませたいファイル名)"とする。
+            if (str.IndexOf("/ItemDB.csv") != -1)
+            {
+                Debug.Log("CSVファイルがあった!!!");
+                //　Asset直下から読み込む（Resourcesではないので注意）
+                TextAsset textasset = AssetDatabase.LoadAssetAtPath<TextAsset>(str);
+                //　同名のScriptableObjectファイルを読み込む。ない場合は新たに作る。
+                string assetfile = str.Replace(".csv", ".asset");
+                ItemDataBase cd = AssetDatabase.LoadAssetAtPath<ItemDataBase>(assetfile);
+                if (cd == null)
+                {
+                    cd = new ItemDataBase();
+                    AssetDatabase.CreateAsset(cd, assetfile);
+                }
+
+                cd.datas = CSVSerializer.Deserialize<ItemData>(textasset.text);
+                EditorUtility.SetDirty(cd);
+                AssetDatabase.SaveAssets();
+            }
+        }
+    }
+}
+#endif
