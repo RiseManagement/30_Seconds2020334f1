9bb592621e4f5acd62614059d5f00c5238d86843
nokeneru <jamihora@outlook.com>
2023-06-16 00:34:34 +0900

    プレイヤー作成、Userクラス作成

---
+++ Assets/User.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class User : MonoBehaviour
+{
+    string name;
+    string Scenename;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scenename=SceneManager.GetActiveScene().name;
+        if(Scenename=="maingame_A")
+        {
+            Debug.Log("name=PlayerA");
+        }
+        else if(Scenename=="maingame_B")
+        {
+            Debug.Log("name=PlayerB");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
