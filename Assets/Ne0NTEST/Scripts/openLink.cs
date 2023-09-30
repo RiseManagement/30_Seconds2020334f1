ece8bc1135cd81b89ee1e257eab17280b1207b09
LuGeNe0N <lie10210@gmail.com>
2023-09-30 17:23:00 +0900

    タイトル仮置き

---
+++ Assets/Ne0NTEST/Scripts/openLink.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openLink : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("clicked");
+        var uri = new System.Uri("https://dubd8946.wixsite.com/risegameteam");
+        Application.OpenURL(uri.AbsoluteUri);
+    }
+}
