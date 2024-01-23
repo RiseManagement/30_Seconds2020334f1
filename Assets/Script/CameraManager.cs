755330a86f5328993fe69234b6f469683f30a2ae
浜ちゃん <hiroakipokemon@gmail.com>
2024-01-23 23:56:04 +0900

    修正：フォーカスキャンセル時元の位置に戻る

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -5,16 +5,16 @@ using UnityEngine.UI;
 
 public class CameraManager : MonoBehaviour
 {
-    private float x;
-    private float y;
+    Vector3 cameraPos;
     private Camera mainCam;
+    public bool Focusflg;
     [SerializeField] GameObject FocusCancelButton;
     [SerializeField] GameObject LButtonActive;
     [SerializeField] GameObject RButtonActive;
     // Start is called before the first frame update
     void Start()
     {
-        x = transform.position.x;
+        Focusflg = false;
         mainCam = Camera.main;
         FocusCancelButton.SetActive(false) ;
         LButtonActive.SetActive(true);
@@ -24,12 +24,13 @@ public class CameraManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CameraPosUpdate();
     }
+
     public void LButton()   //LButtonを押したときの動作
     {
-        x = transform.position.x;
-        if (x<1)
+        cameraPos.x = transform.position.x;
+        if (cameraPos.x < 1)
         {
             transform.Translate(60, 0, 0, Space.World);
             Debug.Log("LButton_if");
@@ -43,8 +44,8 @@ public class CameraManager : MonoBehaviour
     }
     public void RButton()   //RButtonを押したときの動作
     {
-        x = transform.position.x;
-        if(x>59)
+        cameraPos.x = transform.position.x;
+        if(cameraPos.x > 59)
         {
             transform.Translate(-60, 0, 0, Space.World);
         }
@@ -57,25 +58,50 @@ public class CameraManager : MonoBehaviour
         //transform.Rotate(0,-90,0,Space.World);
     }
 
-    public void ItemFocus(Vector2 vector2,int a)//フォーカス機能＋フォーカスボタン表示
+    /// <summary>
+    /// フォーカスではない時座標更新
+    /// </summary>
+    void CameraPosUpdate()
     {
+        if (!Focusflg)
+        {
+            cameraPos.x = transform.position.x;
+            cameraPos.y = transform.position.y;
+        }
+    }
+
+    /// <summary>
+    /// アイテムフォーカス
+    /// </summary>
+    /// <param name="vector2">アイテム座標</param>
+    /// <param name="size">フォーカスサイズ</param>
+    public void ItemFocus(Vector2 vector2,int size)//フォーカス機能＋フォーカスボタン表示
+    {
+        Focusflg = true;
         FocusTransform(vector2);
-        FocusSize(a);
+        SetFocusSize(size);
         FocusCancelButton.SetActive(true);
         LButtonActive.SetActive(false);
         RButtonActive.SetActive(false);
     }
 
-    //引数がアイテムクリック時のアイテム座標のフォーカス機能
+    /// <summary>
+    /// フォーカス座標
+    /// </summary>
+    /// <param name="vector2">アイテム座標</param>
     private void FocusTransform(Vector2 vector2)
     {
         Vector2 Focus_adjust = new Vector2(0, 0);    //フォーカスする位置の調整用Vector2
         transform.position = vector2+Focus_adjust ;
     }
 
-    private void FocusSize(int a)
+    /// <summary>
+    /// フォーカスズーム設定
+    /// </summary>
+    /// <param name="size">フォーカスサイズ</param>
+    private void SetFocusSize(int size)
     {
-        switch(a)
+        switch(size)
         {
             case 1://ズーム小
                 mainCam.orthographicSize = 4;
@@ -91,32 +117,17 @@ public class CameraManager : MonoBehaviour
                 break;
         }   
     }
+
+    /// <summary>
+    /// フォーカスキャンセル
+    /// </summary>
     public void FocusCancel()
     {
         mainCam.orthographicSize = 5;
-        y=transform.position.y;
+        mainCam.transform.position = cameraPos;
+        Focusflg = false;
         FocusCancelButton.SetActive(false);
         LButtonActive.SetActive(true);
         RButtonActive.SetActive(true);
-        if (y>-10&&y<10)
-        {
-            mainCam.transform.position = new Vector2(0,0);
-        }
-        else if(y>10&&y<30)
-        {
-            mainCam.transform.position = new Vector2(0, 20);
-        }
-        else if (y > 30 && y < 50)
-        {
-            mainCam.transform.position = new Vector2(0, 40);
-        }
-        else if (y > 50 && y < 70)
-        {
-            mainCam.transform.position = new Vector2(0, 60);
-        }
-        else
-        {
-            Debug.Log("FocusCancel失敗");
-        }
     }
 }
